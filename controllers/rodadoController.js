11d56458ed10e8fd5783b4dcca9000c2371dc4ac
Rodrigo Calgaro <ri.calgaro@gmail.com>
2019-07-21 12:41:15 -0300

    CRUD Rodados

--- controllers/rodadoController.js
+++ controllers/rodadoController.js
@@ -1,14 +1,13 @@
 const Totales = require("./../utils/totales")
+const Movimientos = require("./../utils/movimientos")
 const models = require("../models");
 const Rodado = models.Rodado;
 const Caja = models.Caja;
 const Banco = models.Banco;
-const Inversion = models.Inversion;
 
 var rodadoController = {};
 
 rodadoController.list = function (req, res) {
-  var valorTotal = 0
   Rodado.findAll({
     where: {
       empresaId: res.locals.empresa.empresaId
@@ -50,25 +49,40 @@ rodadoController.list = function (req, res) {
 };
 
 rodadoController.add = function (req, res) {
+  reqMC = req.body.movimientoCompra
   Rodado.create(req.body.rodado).then(rodado => {
+    if (req.body.tipoAlta == 'compra') {
+      reqMC.forEach(async mc => {
+        await Movimientos.compra(rodado.fechaCompra, mc.monto, 'Rodado', rodado.rodadoId, mc.cuenta, mc.cuentaId, rodado.empresaId)
+      });
+    }
       req.flash("success_msg", "Se dio de alta un rodado correctamente");
       res.redirect("/rodados");
     })
     .catch(error => {
+      console.log(error)
       req.flash("error_msg", "Error al dar de alta un rodado");
       res.redirect("/rodados");
     });
 };
 
 rodadoController.saveEdit = function (req, res) {
+  reqMC = req.body.movimientoCompra
   Rodado.findByPk(req.params.id).then(rodado => {
     rodado
       .update(req.body.rodado)
       .then(() => {
+        if (req.body.tipoAlta == 'compra') {
+          Movimientos.deshacerCompra('Rodado', req.params.id)
+          reqMC.forEach(mc => {
+            Movimientos.compra(rodado.fechaCompra, mc.monto,'Rodado', rodado.rodadoId, mc.cuenta , mc.cuentaId, rodado.empresaId) 
+          });
+        }
         req.flash("success_msg", "Se actualizó un rodado correctamente");
         res.redirect("/rodados");
       })
       .catch(err => {
+        console.log(err)
         req.flash("error_msg", "Error al actualizar un rodado");
         res.redirect("/rodados");
       });
@@ -80,11 +94,13 @@ rodadoController.delete = function (req, res) {
       where: {
         rodadoId: req.params.id
       }
-    }).then(() => {
+    }).then(async () => {
+      await Movimientos.deshacerCompra('Rodado', req.params.id)
       req.flash("success_msg", "Se dio de baja un rodado correctamente");
       res.redirect("/rodados");
     })
     .catch(err => {
+      console.log(err)
       req.flash("error_msg", "Error al dar de baja un rodado");
       res.redirect("/rodados");
     });
@@ -92,91 +108,56 @@ rodadoController.delete = function (req, res) {
 
 rodadoController.liquidar = function (req, res) {
   var reqRodado = req.body.rodado
-    console.log(reqRodado)
-  if (reqRodado.cuentaVenta == 'Caja') {
+  var reqMovimiento = req.body.movimiento
+  
+  if (reqMovimiento.cuenta == 'Caja') {
     Caja.findOne({
       where: {
         empresaId: res.locals.empresa.empresaId
       }
-    }).then(caja => {
-
-      var movimientos = caja.montoMovimientos + parseFloat(reqRodado.valorVenta)
-      Caja.update({
-        montoMovimientos: movimientos
-      }, {
+    }).then(async caja => {
+      
+      await Movimientos.venta(reqRodado.fechaVenta, reqMovimiento.monto, 'Rodado', req.params.id, reqMovimiento.cuenta, caja.cajaId, res.locals.empresa.empresaId)
+      
+      Rodado.update(reqRodado, {
         where: {
-          cajaId: caja.cajaId
+          rodadoId: req.params.id
         }
       }).then(() => {
-        Rodado.update(reqRodado, { where: {
-          rodadoId: req.params.id
-        }}).then(() => {
-            req.flash("success_msg", "Se liquidó un rodado correctamente");
-            res.redirect("/rodados");
-          })
-          .catch(err => {
-            req.flash("error_msg", "Error al liquidar un rodado");
-            res.redirect("/rodados");
-          });
+        req.flash("success_msg", "Se liquidó rodado correctamente");
+        res.redirect("/rodados");
       })
+      .catch(err => {
+        console.log(err)
+        req.flash("error_msg", "Error al liquidar rodado");
+        res.redirect("/rodados");
+      });
+      
     })
   }
-
-  if (reqRodado.cuentaVenta == 'Banco') {
+  
+  if (reqMovimiento.cuenta == 'Banco') {
     Banco.findOne({
       where: {
         empresaId: res.locals.empresa.empresaId
       }
-    }).then(banco => {
-
-      var movimientos = banco.montoMovimientos + parseFloat(reqRodado.valorVenta)
-      Banco.update({
-        montoMovimientos: movimientos
-      }, {
+    }).then(async banco => {
+      
+      await Movimientos.venta(reqRodado.fechaVenta, reqMovimiento.monto, 'Rodado', req.params.id, reqMovimiento.cuenta, banco.bancoId, res.locals.empresa.empresaId)
+      
+      Rodado.update(reqRodado, {
         where: {
-          bancoId: banco.bancoId
+          rodadoId: req.params.id
         }
       }).then(() => {
-        Rodado.update(reqRodado, { where: {
-          rodadoId: req.params.id
-        }}).then(() => {
-            req.flash("success_msg", "Se liquidó un rodado correctamente");
-            res.redirect("/rodados");
-          })
-          .catch(err => {
-            req.flash("error_msg", "Error al liquidar un rodado");
-            res.redirect("/rodados");
-          });
+        req.flash("success_msg", "Se liquidó rodado correctamente");
+        res.redirect("/rodados");
       })
-    })
-  }
-
-  if (reqRodado.cuentaVenta == 'Inversion') {
-    Inversion.findOne({
-      where: {
-        empresaId: res.locals.empresa.empresaId
-      }
-    }).then(inversion => {
-
-      var movimientos = inversion.montoMovimientos + parseFloat(reqRodado.valorVenta)
-      Inversion.update({
-        montoMovimientos: movimientos
-      }, {
-        where: {
-          inversionId: inversion.inversionId
-        }
-      }).then(() => {
-        Rodado.update(reqRodado, { where: {
-          rodadoId: req.params.id
-        }}).then(() => {
-            req.flash("success_msg", "Se liquidó un rodado correctamente");
-            res.redirect("/rodados");
-          })
-          .catch(err => {
-            req.flash("error_msg", "Error al liquidar un rodado");
-            res.redirect("/rodados");
-          });
-      })
+      .catch(err => {
+        console.log(err)
+        req.flash("error_msg", "Error al liquidar rodados");
+        res.redirect("/rodados");
+      });
     })
   }
 
@@ -184,104 +165,22 @@ rodadoController.liquidar = function (req, res) {
 
 
 rodadoController.deshacerLiquidar = function (req, res) {
-  Rodado.findByPk(req.params.id).then(rodado => {
-    if (rodado.cuentaVenta == 'Caja') {
-      Caja.findOne({
-        where: {
-          empresaId: rodado.empresaId
-        }
-      }).then(caja => {
-        var movimientos = caja.montoMovimientos - parseFloat(rodado.valorVenta)
-        Caja.update({
-          montoMovimientos: movimientos
-        }, {
-          where: {
-            cajaId: caja.cajaId
-          }
-        }).then(() => {
-          Rodado.update({
-            cuentaVenta: null,
-            valorVenta: null,
-            fechaVenta: null
-          }, { where: {
-            rodadoId: rodado.rodadoId
-          }}).then(() => {
-              req.flash("success_msg", "Se liquidó un rodado correctamente");
-              res.redirect("/rodados");
-            })
-            .catch(err => {
-              req.flash("error_msg", "Error al liquidar un rodado");
-              res.redirect("/rodados");
-            });
-        })
-      })
+  Rodado.update({
+    fechaVenta: null
+  }, {
+    where: {
+      rodadoId: req.params.id
     }
-  
-    if (rodado.cuentaVenta == 'Banco') {
-      Banco.findOne({
-        where: {
-          empresaId: rodado.empresaId
-        }
-      }).then(banco => {
-        var movimientos = banco.montoMovimientos - parseFloat(rodado.valorVenta)
-        Banco.update({
-          montoMovimientos: movimientos
-        }, {
-          where: {
-            bancoId: banco.bancoId
-          }
-        }).then(() => {
-          Rodado.update({
-            cuentaVenta: null,
-            valorVenta: null,
-            fechaVenta: null
-          }, { where: {
-            rodadoId: rodado.rodadoId
-          }}).then(() => {
-              req.flash("success_msg", "Se liquidó un rodado correctamente");
-              res.redirect("/rodados");
-            })
-            .catch(err => {
-              req.flash("error_msg", "Error al liquidar un rodado");
-              res.redirect("/rodados");
-            });
-        })
-      })
-    }
-  
-    if (rodado.cuentaVenta == 'Inversion') {
-      Inversion.findOne({
-        where: {
-          empresaId: rodado.empresaId
-        }
-      }).then(inversion => {
-        var movimientos = inversion.montoMovimientos - parseFloat(rodado.valorVenta)
-        Inversion.update({
-          montoMovimientos: movimientos
-        }, {
-          where: {
-            inversionId: inversion.inversionId
-          }
-        }).then(() => {
-          Rodado.update({
-            cuentaVenta: null,
-            valorVenta: null,
-            fechaVenta: null
-          }, { where: {
-            rodadoId: rodado.rodadoId
-          }}).then(() => {
-              req.flash("success_msg", "Se deshizo la liquidación de un rodado correctamente");
-              res.redirect("/rodados");
-            })
-            .catch(err => {
-              req.flash("error_msg", "Error al deshacer la liquidación de un rodado");
-              res.redirect("/rodados");
-            });
-        })
-      })
-    }
+  }).then(async () => {
+    await Movimientos.deshacerVenta('Rodado', req.params.id)
+    req.flash("success_msg", "Se deshizo la liquidación de rodado correctamente");
+    res.redirect("/rodados");
   })
-
+  .catch(err => {
+    console.log(err)
+    req.flash("error_msg", "Error al deshacer liquidación de rodado");
+    res.redirect("/rodados");
+  });
 };
 
 
--- public/scripts/public-rodados.js
+++ public/scripts/public-rodados.js
@@ -16,6 +16,7 @@ rodados.forEach(function (row) {
         vista.querySelector("#conceptoRodado").value = rodado.concepto;
         vista.querySelector("#fechaCompraRodado").value = rodado.fechaCompra;
         vista.querySelector("#cantidadRodado").value = rodado.cantidad;
+        vista.querySelector("#valorRodado").value = rodado.valorUnitario;
         vista.querySelector("#valorMercadoRodado").value = rodado.valorMercado;
         vista.querySelector("#vidaUtilRodado").value = rodado.vidaUtil;
         vista.querySelector("#antiguedadRodado").value = rodado.antiguedad;
@@ -37,10 +38,33 @@ rodados.forEach(function (row) {
             vista.querySelector("#conceptoRodado").value = rodado.concepto;
             vista.querySelector("#fechaCompraRodado").value = rodado.fechaCompra;
             vista.querySelector("#cantidadRodado").value = rodado.cantidad;
-            vista.querySelector("#valorMercadoRodado").value = rodado.valorMercado;
+            vista.querySelector("#valorRodado").value = rodado.valorUnitario;
             vista.querySelector("#vidaUtilRodado").value = rodado.vidaUtil;
             vista.querySelector("#estadoRodado").value = rodado.estado;
             vista.querySelector("#valorResidualRodado").value = rodado.valorResidual;
+
+            axios.get('/apiMovimientos/getCompras/' + rodado.empresaId + '/Rodado/' + rodado.rodadoId).then(res => {
+                var movimientos = res.data.movimientos
+                if (movimientos.length > 0) {
+                    radioCapital.checked = false
+                    radioCompra.checked = true
+
+                    MC_updateFechaDeCompra(rodado.fechaCompra)
+                    MostrarCompra()
+                    actualizarValorTotal()
+                    if (i < movimientos.length) {}
+                    for (let i = 1; i < movimientos.length; i++) {
+                        MC_addLineaCompra()
+                    }
+                    for (let i = 0; i < movimientos.length; i++) {
+                        const movimiento = movimientos[i];
+                        vista.querySelector("#MC_MedioDePago-" + i).value = movimiento.cuenta
+                        vista.querySelector("#MC_CuentaId-" + i).value = movimiento.cuentaId
+                        vista.querySelector("#MC_Monto-" + i).value = movimiento.monto
+                    }
+                    MC_updateSumaPagos()
+                }
+            })
         });
     }
 
@@ -78,4 +102,46 @@ rodados.forEach(function (row) {
 function getElement(row) {
     var element = JSON.parse(row.attributes["data"].value);
     return element;
+}
+
+function MostrarCapital() {
+    var fechaCompraRodado = vista.querySelector("#fechaCompraRodado")
+    fechaCompraRodado.min = null
+    fechaCompraRodado.max = datosEmpresa.inicioEjercicio
+    ocultarMediosPago()
+}
+
+function MostrarCompra() {
+    var fechaCompraRodado = vista.querySelector("#fechaCompraRodado")
+    fechaCompraRodado.min = datosEmpresa.inicioEjercicio
+    fechaCompraRodado.max = datosEmpresa.finEjercicio
+    MC_updateFechaDeCompra(datosEmpresa.inicioEjercicio)
+    mostrarMediosPago()
+}
+
+function actualizarValorTotal() {
+    var cantidad = vista.querySelector("#cantidadRodado").value
+    var valorUnidad = vista.querySelector("#valorRodado").value
+    if (cantidad) {
+        cantidad = parseFloat(cantidad)
+    } else {
+        cantidad = 0
+    }
+
+    if (valorUnidad) {
+        valorUnidad = parseFloat(valorUnidad)
+    } else {
+        valorUnidad = 0
+    }
+
+    updateTotalAPagar(cantidad * valorUnidad)
+}
+
+function validateForm() {
+    var radioCompra = vista.querySelector("#radioCompra");
+    if (radioCompra.checked) {
+        return validarPagos()
+    } else {
+        return true
+    }
 }
